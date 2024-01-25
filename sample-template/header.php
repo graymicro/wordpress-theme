034f4be25235268258b34e926271ce3e6dfc0273
graymicro <alirezaallahyarian@gmail.com>
2024-01-25 11:56:33 +0330

    commit

--- sample-template/header.php
+++ sample-template/header.php
@@ -5,27 +5,30 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Multi-Purpose Creative Agency HTML5 Template - Roxine</title>
+    <!-- <title>Multi-Purpose Creative Agency HTML5 Template - Roxine</title> -->
     <!-- Font Icons -->
-    <link media="all" rel="stylesheet" href="css/fonts/icomoon/icomoon.css">
-<!--     <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css"> -->
-    <link media="all" rel="stylesheet" href="vendors/font-awesome/css/font-awesome.css">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/fonts/icomoon/icomoon.css'?>">
+<!--     <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css'?>"> -->
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/vendors/font-awesome/css/font-awesome.css'?>">
     <!-- Vendors -->
-    <link media="all" rel="stylesheet" href="vendors/owl-carousel/dist/assets/owl.carousel.min.css">
-    <link media="all" rel="stylesheet" href="vendors/owl-carousel/dist/assets/owl.theme.default.min.css">
-    <link media="all" rel="stylesheet" href="vendors/animate/animate.css">
-    <link media="all" rel="stylesheet" href="vendors/rateyo/jquery.rateyo.css">
-    <link media="all" rel="stylesheet" href="vendors/bootstrap-datepicker/css/bootstrap-datepicker.css">
-    <link media="all" rel="stylesheet" href="vendors/fancyBox/source/jquery.fancybox.css">
-    <link media="all" rel="stylesheet" href="vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/owl-carousel/dist/assets/owl.carousel.min.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/owl-carousel/dist/assets/owl.theme.default.min.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/animate/animate.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/rateyo/jquery.rateyo.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/jquery.fancybox.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css'?>">
     <!-- Bootstrap 4 -->
-    <link media="all" rel="stylesheet" href="css/bootstrap.css">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/bootstrap.css'?>">
     <!-- Rev Slider -->
-    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/settings.css">
-    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/layers.css">
-    <link rel="stylesheet" type="text/css" href="vendors/rev-slider/revolution/css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/settings.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/layers.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/navigation.css'?>">
     <!-- Theme CSS -->
     <link media="all" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <!-- Custom CSS -->
-    <link media="all" rel="stylesheet" href="css/custom.css">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/custom.css'; ?>">
+    <?php wp_head(); ?>
 </head>
+
+<body class="white-overlay">
