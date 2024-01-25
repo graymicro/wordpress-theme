1cd4d8c065f9972e5030bf62c9cc722e3ad90c0c
graymicro <alirezaallahyarian@gmail.com>
2024-01-25 12:42:41 +0330

    commit

---
+++ sample-template/footer.php
@@ -0,0 +1,45 @@
+    <!-- jQuery Library -->
+    <script src="<?php echo get_template_directory_uri().'/vendors/jquery/jquery-2.1.4.min.js'?>"></script>
+    <!-- Vendor Scripts -->
+    <script src="<?php echo get_template_directory_uri().'/vendors/tether/dist/js/tether.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/bootstrap/js/bootstrap.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/stellar/jquery.stellar.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/isotope/javascripts/isotope.pkgd.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/isotope/javascripts/packery-mode.pkgd.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/owl-carousel/dist/owl.carousel.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/waypoint/waypoints.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/counter-up/jquery.counterup.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/jquery.fancybox.pack.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/image-stretcher-master/image-stretcher.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/wow/wow.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/rateyo/jquery.rateyo.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/bootstrap-datepicker/js/bootstrap-datepicker.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/bootstrap-slider-master/src/js/bootstrap-slider.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/bootstrap-select/dist/js/bootstrap-select.min.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/mega-menu.js'?>"></script>
+    <script src="<?php echo get_template_directory_uri().'/vendors/retina/retina.min.js'?>"></script>
+    <!-- Custom Script -->
+    <script src="<?php echo get_template_directory_uri().'/js/jquery.main.js'?>"></script>
+    <!-- REVOLUTION JS FILES -->
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/jquery.themepunch.tools.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/jquery.themepunch.revolution.min.js'?>"></script>
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.actions.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.carousel.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.kenburn.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.layeranimation.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.migration.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.navigation.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.parallax.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.slideanims.min.js'?>"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/js/extensions/revolution.extension.video.min.js'?>"></script>
+    <!-- SNOW ADD ON -->
+    <script type="text/javascript" src="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution-addons/snow/revolution.addon.snow.min.js'?>"></script>
+    <!-- Revolution Slider Script -->
+    <script src="<?php echo get_template_directory_uri().'/js/revolution.js'?>"></script>
+    
+    <?php wp_footer();?>
+</body>
+
+</html>
