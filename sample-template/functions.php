84563bd21a98b86b31d743a3613d8b0af8e8aff9
graymicro <alirezaallahyarian@gmail.com>
2024-01-25 13:16:26 +0330

    commit

--- sample-template/functions.php
+++ sample-template/functions.php
@@ -1,3 +1,21 @@
 <?php
 
 add_theme_support( 'title-tag' );
+
+
+// theme actions 
+
+function dokmeh_test(){
+
+    $menus = array(
+        'main-menu' => 'A Menu For Main Header', 
+        'complete-menu' => 'A Menu For Completing the main menu',
+        'footer-menu' => 'A Menu For Footer'
+
+    );
+
+    register_nav_menus($menus);
+
+}
+
+add_action('after_setup_theme','dokmeh_test');
--- sample-template/header.php
+++ sample-template/header.php
@@ -7,23 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <!-- <title>Multi-Purpose Creative Agency HTML5 Template - Roxine</title> -->
     <!-- Font Icons -->
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/fonts/icomoon/icomoon.css'?>">
-<!--     <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css'?>"> -->
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/vendors/font-awesome/css/font-awesome.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/fonts/icomoon/icomoon.css' ?>">
+    <!--     <link media="all" rel="stylesheet" href="css/fonts/roxine-font-icon/roxine-font.css'?>"> -->
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/vendors/font-awesome/css/font-awesome.css' ?>">
     <!-- Vendors -->
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/owl-carousel/dist/assets/owl.carousel.min.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/owl-carousel/dist/assets/owl.theme.default.min.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/animate/animate.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/rateyo/jquery.rateyo.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/jquery.fancybox.css'?>">
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/owl-carousel/dist/assets/owl.carousel.min.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/owl-carousel/dist/assets/owl.theme.default.min.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/animate/animate.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/rateyo/jquery.rateyo.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/bootstrap-datepicker/css/bootstrap-datepicker.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/fancyBox/source/jquery.fancybox.css' ?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/vendors/fancyBox/source/helpers/jquery.fancybox-thumbs.css' ?>">
     <!-- Bootstrap 4 -->
-    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri().'/css/bootstrap.css'?>">
+    <link media="all" rel="stylesheet" href="<?php echo get_template_directory_uri() . '/css/bootstrap.css' ?>">
     <!-- Rev Slider -->
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/settings.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/layers.css'?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri().'/vendors/rev-slider/revolution/css/navigation.css'?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/vendors/rev-slider/revolution/css/settings.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/vendors/rev-slider/revolution/css/layers.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri() . '/vendors/rev-slider/revolution/css/navigation.css' ?>">
     <!-- Theme CSS -->
     <link media="all" rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
     <!-- Custom CSS -->
@@ -32,3 +32,8 @@
 </head>
 
 <body class="white-overlay">
+    <div class="preloader" id="pageLoad">
+        <div class="holder">
+            <div class="coffee_cup"></div>
+        </div>
+    </div>
